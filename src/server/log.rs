5a4f09a8fe525b230825afba2b1929a75b384cd3
Ulf Lilleengen <lulf@yahoo-inc.com>
2014-10-12 20:00:44 +0200

    Add reference to self

--- src/server/log.rs
+++ src/server/log.rs
@@ -12,10 +12,10 @@ pub enum Operation {
 
 
 pub trait Log {
-    fn write(term: uint, operation: Operation) -> IoResult<uint>;
-    fn read(term: uint, index: uint) -> IoResult<Operation>;
-    fn last_index() -> IoResult<uint>;
-    fn log_term() -> IoResult<uint>;
+    fn write(&self, term: uint, operation: Operation) -> IoResult<uint>;
+    fn read(&self, term: uint, index: uint) -> IoResult<Operation>;
+    fn last_index(&self) -> IoResult<uint>;
+    fn log_term(&self) -> IoResult<uint>;
 }
 
 pub struct MemoryLog;
@@ -27,16 +27,16 @@ impl MemoryLog {
 }
 
 impl Log for MemoryLog {
-    fn write(term: uint, operation: Operation) -> IoResult<uint> {
+    fn write(&self, term: uint, operation: Operation) -> IoResult<uint> {
         Err(IoError{ kind: InvalidInput, desc: "Not yet implemented", detail: None})
     }
-    fn read(term: uint, index: uint) -> IoResult<Operation> {
+    fn read(&self, term: uint, index: uint) -> IoResult<Operation> {
         Err(IoError{ kind: InvalidInput, desc: "Not yet implemented", detail: None})
     }
-    fn last_index() -> IoResult<uint> {
+    fn last_index(&self) -> IoResult<uint> {
         Err(IoError{ kind: InvalidInput, desc: "Not yet implemented", detail: None})
     }
-    fn log_term() -> IoResult<uint> {
+    fn log_term(&self) -> IoResult<uint> {
         Err(IoError{ kind: InvalidInput, desc: "Not yet implemented", detail: None})
     }
 }
